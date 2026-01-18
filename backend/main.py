b01cb4adc988e2177d9b3938bd9fbd13933182d4
christophe gabriel <christopheguerra67@gmail.com>
2026-01-18 16:33:27 -0300

    fix:changing cors

--- backend/main.py
+++ backend/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins = ["http://localhost:3001","http://localhost:3000"],
+    allow_origins = ["http://localhost:3001","http://localhost:3000","https://dna-explorer-theta.vercel.app"],
     allow_methods = ["*"],
     allow_headers = ["*"]
 )
